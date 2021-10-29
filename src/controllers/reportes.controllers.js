ae8bf8b5fa7519a617db3de06a34b154b34da5d3
ViktorG04 <61758307+ViktorG04@users.noreply.github.com>
2021-10-28 20:41:23 -0600

    validaciones en areas, crup incapacidades, store prodecude reportes y reportes controller

---
+++ src/controllers/reportes.controllers.js
@@ -0,0 +1,41 @@
+import { getConnection, querys } from "../database";
+
+//all areas
+export const getAllReportes = async (req, res) => {
+
+    var resultS, resulPS;
+
+    resultS = await consultReportes('LT');
+
+    resulPS = await consultReportes('LS')
+
+    for(const i in resulPS){
+        for(const x in resultS){
+            if(resultS[x]['idSolicitud'] == resulPS[i]['idSolicitud']){
+                if(resultS[x]['idDetalle'] == resulPS[i]['idDetalle']){
+                    resultS[x].temperatura = resulPS[i]['temperatura'];
+                    resultS[x].fechaHoraIngreso = resulPS[i]['fechaHoraIngreso'];
+                    resultS[x].fechaHoraSalida = resulPS[i]['fechaHoraSalida'];
+                }
+            }
+        }
+    }
+
+    res.json(resultS);
+};
+
+
+//function crup in database
+async function consultReportes(action) {
+  try {
+    const connection = await getConnection();
+    const result = await connection
+      .request()
+      .input("A", action)
+      .query(querys.getReportes);
+      return result.recordset;
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
